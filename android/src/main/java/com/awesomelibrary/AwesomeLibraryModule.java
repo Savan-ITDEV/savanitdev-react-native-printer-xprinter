bcfe2775ee93871ce24e8d5df38328953b5391b7
savanitdev@gmail.com <savanitdev@mail.comgit config --global user.email savanitdev@gmail.com>
2023-11-16 19:39:23 +0700

    update

--- android/src/main/java/com/awesomelibrary/AwesomeLibraryModule.java
+++ android/src/main/java/com/awesomelibrary/AwesomeLibraryModule.java
@@ -1,197 +1,170 @@
 package com.awesomelibrary;
+import static android.content.Context.BIND_AUTO_CREATE;
+
+import android.content.ComponentName;
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
+import android.os.IBinder;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.module.annotations.ReactModule;
-import net.posprinter.IDeviceConnection;
-import net.posprinter.IPOSListener;
-import net.posprinter.POSConnect;
-import net.posprinter.POSConst;
-import net.posprinter.POSPrinter;
+import java.util.ArrayList;
+import java.util.List;
+import net.posprinter.posprinterface.IMyBinder;
+import net.posprinter.posprinterface.ProcessData;
+import net.posprinter.posprinterface.TaskCallback;
+import net.posprinter.service.PosprinterService;
+import net.posprinter.utils.BitmapProcess;
+import net.posprinter.utils.BitmapToByteData;
+import net.posprinter.utils.DataForSendToPrinterPos80;
+
 
 @ReactModule(name = AwesomeLibraryModule.NAME)
 public class AwesomeLibraryModule extends ReactContextBaseJavaModule {
-  public static final String NAME = "AwesomeLibrary";
-  Context context;
-  public AwesomeLibraryModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    context = reactContext;
-  }
-  private IPOSListener connectListener = new IPOSListener() {
+    public static final String NAME = "AwesomeLibrary";
+    public static IMyBinder myBinder;
+    public static boolean ISCONNECT=false;
+    Context context;
+    public AwesomeLibraryModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        context = reactContext ;
+    }
+
     @Override
-    public void onStatus(int code, String msg) {
-      SharedPreferences.Editor editor = context.getSharedPreferences("MyPrefsFile", Context.MODE_PRIVATE).edit();
-      switch (code) {
-        case POSConnect.CONNECT_SUCCESS:
-          Log.e("Notify", "CONNECT_SUCCESS");
-
-          editor.putString("status", "CONNECT_SUCCESS");
-          editor.apply();
-          break;
-        case POSConnect.CONNECT_FAIL:
-          Log.e("Notify", "CONNECT_FAIL");
-
-          editor.putString("status", "CONNECT_FAIL");
-          editor.apply();
-          break;
-        case POSConnect.CONNECT_INTERRUPT:
-          Log.e("Notify", "CONNECT_INTERRUPT");
-
-          editor.putString("status", "CONNECT_INTERRUPT");
-          editor.apply();
-          break;
-        case POSConnect.SEND_FAIL:
-          Log.e("Notify", "SEND_FAIL");
-
-          editor.putString("status", "SEND_FAIL");
-          editor.apply();
-          break;
-        case POSConnect.USB_DETACHED:
-          Log.e("Notify", "USB_DETACHED");
-
-          editor.putString("status", "USB_DETACHED");
-          editor.apply();
-          break;
-        case POSConnect.USB_ATTACHED:
-          Log.e("Notify", "USB_ATTACHED");
-
-          editor.putString("status", "USB_ATTACHED");
-          editor.apply();
-          break;
-      }
-
+    @NonNull
+    public String getName() {
+        return NAME;
     }
-  };
-  @Override
-  @NonNull
-  public String getName() {
-    return NAME;
-  }
 
-
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a - b);
-  }
-  @ReactMethod
-  public void onCreate() {
-    Log.e("App Notify", "init Done!");
-    POSConnect.init(context);
-  }
-  IDeviceConnection curConnect = null;
-  @ReactMethod
-  public void connectNet(String ipAddress,Promise promise) {
-    SharedPreferences prefs = context.getSharedPreferences("MyPrefsFile", Context.MODE_PRIVATE);
-    String str = prefs.getString("status", "");
-    try {
-      if (curConnect != null) {
-        curConnect.close();
-      }
-      curConnect = POSConnect.createDevice(POSConnect.DEVICE_TYPE_ETHERNET);
-      curConnect.connect(ipAddress, connectListener);
-
-      Log.e("App Notify", "connect printer Done" + str);
-      promise.resolve(str);
-    } catch (Exception e) {
-      Log.e("App Notify", "connect printer fail!");
-      promise.resolve(str);
-
-    }
-  }
-
-  @ReactMethod
-  public void printText() {
-    try {
-      POSPrinter printer = new POSPrinter(curConnect);
-      printer.initializePrinter().feedLine();
-      Log.e("App Notify", "connect printer Done" );
-
-    } catch (Exception e) {
-      Log.e("App Notify", "connect printer fail!");
-    }
-  }
-  @ReactMethod
-  public void disConnectNet(Promise promise) {
-    try {
-      curConnect.close();
-      Log.e("App Notify", "disconnect printer Done");
-      promise.resolve(true);
-    } catch (Exception e) {
-      Log.e("App Notify", "disconnect printer fail!");
-      promise.resolve(false);
-    }
-  }
-
-   @ReactMethod
-    public void connectBt(String macAddress,Promise promise) {
-        SharedPreferences prefs = context.getSharedPreferences("MyPrefsFile", Context.MODE_PRIVATE);
-        String str = prefs.getString("status", "");
-        try {
-        if (curConnect != null) {
-            curConnect.close();
+    ServiceConnection mSerconnection= new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            myBinder= (IMyBinder) service;
+            Log.e("myBinder","connect");
+            Toast toast = Toast.makeText(context, "connect", Toast.LENGTH_SHORT);
+            toast.show();
         }
-        Log.e("App Notify",  macAddress);
-        curConnect = POSConnect.createDevice(POSConnect.DEVICE_TYPE_BLUETOOTH);
-        curConnect.connect(macAddress, connectListener);
-            promise.resolve(str);
-        } catch (Exception e) {
-            Log.e("App Notify", "connect printer fail!");
-            promise.resolve(str);
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.e("myBinder","disconnect");
+            Toast toast = Toast.makeText(context, "disconnect", Toast.LENGTH_SHORT);
+            toast.show();
         }
+    };
+
+
+
+    @ReactMethod
+    public void onCreate() {
+        Toast toast = Toast.makeText(context, "init Done!", Toast.LENGTH_SHORT);
+        toast.show();
+//        Log.e("App Notify", "init Done!");
+//        super.onStart();
+
+        Intent intent =new Intent(context, PosprinterService.class);
+        context.bindService(intent,mSerconnection,BIND_AUTO_CREATE);
     }
+
     public static Bitmap decodeBase64ToBitmap(String base64String) {
         byte[] decodedBytes = Base64.decode(base64String, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
     }
-    public static Bitmap changeBitmapContrastBrightness(Bitmap bmp, float contrast, float brightness)
-{
-    ColorMatrix cm = new ColorMatrix(new float[]
-            {
-                contrast, 0, 0, 0, brightness,
-                0, contrast, 0, 0, brightness,
-                0, 0, contrast, 0, brightness,
-                0, 0, 0, 1, 0
-            });
-
-    Bitmap ret = Bitmap.createBitmap(bmp.getWidth(), bmp.getHeight(), bmp.getConfig());
-
-    Canvas canvas = new Canvas(ret);
-
-    Paint paint = new Paint();
-    paint.setColorFilter(new ColorMatrixColorFilter(cm));
-    canvas.drawBitmap(bmp, 0, 0, paint);
-
-    return ret;
-}
     @ReactMethod
-    public void printImg(String base64String,float contrast, float brightness) {
-        try {
-            // Decode Base64 string to Bitmap
-            Bitmap bitmap = decodeBase64ToBitmap(base64String);
-            POSPrinter printer = new POSPrinter(curConnect);
-            printer.
-                    initializePrinter()
-                    .printBitmap(changeBitmapContrastBrightness(bitmap,contrast,brightness),POSConst.ALIGNMENT_CENTER, 500)
-                    .feedLine();
-//                    .cutHalfAndFeed(1);
-            Log.e("App Notify", "connect printer Done" );
-        } catch (Exception e) {
-            Log.e("App Notify", "connect printer fail!");
+    private void disConnect(){
+        if (ISCONNECT){
+            myBinder.DisconnectCurrentPort(new TaskCallback() {
+                @Override
+                public void OnSucceed() {
+                    ISCONNECT = false;
+
+                }
+
+                @Override
+                public void OnFailed() {
+                    ISCONNECT = true;
+
+                }
+            });
         }
     }
-  
+
+    @ReactMethod
+    private void connectNet(String ip,String base64,int w1,int w2){
+
+        if (ip!=null){
+            if (ISCONNECT) {
+                myBinder.DisconnectCurrentPort(new TaskCallback() {
+                    @Override
+                    public void OnSucceed() {
+                    }
+
+                    @Override
+                    public void OnFailed() {
+                        disConnect();
+                    }
+                });
+            } else {
+                myBinder.ConnectNetPort(ip, 9100, new TaskCallback() {
+                    @Override
+                    public void OnSucceed() {
+                        ISCONNECT = true;
+                        printBitmap(base64,w1,w2);
+                        Log.e("App Notify connectNet3", "connect OnSucceed" );
+                    }
+
+                    @Override
+                    public void OnFailed() {
+                        ISCONNECT = false;
+                        Log.e("App Notify connectNet3", "connect OnFailed" );
+                        disConnect();
+                    }
+                });
+            }
+
+        }else {
+            Log.e("App Notify connectNet3", "connect OnFailed 2" );
+        }
+    }
+
+    private void printBitmap(String base64String,int w1,int w2){
+//        final Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.test);
+        final Bitmap bitmap1 =  BitmapProcess.compressBmpByYourWidth
+                (decodeBase64ToBitmap(base64String),w1);
+
+        if (ISCONNECT){
+            myBinder.WriteSendData(new TaskCallback() {
+                @Override
+                public void OnSucceed() {
+                    disConnect();
+
+                }
+
+                @Override
+                public void OnFailed() {
+                    disConnect();
+                }
+            }, new ProcessData() {
+                @Override
+                public List<byte[]> processDataBeforeSend() {
+                    List<byte[]> list = new ArrayList<>();
+                    list.add(DataForSendToPrinterPos80.initializePrinter());
+
+                    list.add(DataForSendToPrinterPos80.printRasterBmp(0,bitmap1, BitmapToByteData.BmpType.Dithering, BitmapToByteData.AlignType.Center,w2));
+
+                    list.add(DataForSendToPrinterPos80.printAndFeedLine());
+                    return list;
+                }
+            });
+        }else {
+            disConnect();
+        }
+    }
+
 }
